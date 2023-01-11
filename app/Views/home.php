7416509839c61baedea807734a5e1cef658637c6
Aaron Will Djaba <papiliocurtis@gmail.com>
2023-01-11 00:24:22 +0000

    Updated site meta data

--- app/Views/home.php
+++ app/Views/home.php
@@ -6,7 +6,10 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
     <title>247 Technologies | Home</title>
-    <meta content="" name="description">
+    <meta property="og:image" content="<?= base_url(); ?>/public/img/hero-img.png"/>
+    <meta content="24/7 Technologies is a global intent technology company that has combined resources
+          (Skills,abilities, Intelligence, etc) as a tool in the Interplanetary Developmental programs."
+          name="description">
     <meta content="" name="keywords">
 
     <!-- Favicons -->
